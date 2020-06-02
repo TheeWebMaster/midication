0b6ff7985af70793212e1cbe3a25aa4844afd4f7
yossri mhamdi <bavary1515@gmail.com>
2020-06-02 01:15:48 +0100

    git test.

--- modules/historique/clear_prev_history_files.py
+++ modules/historique/clear_prev_history_files.py
@@ -4,5 +4,4 @@ def clear_prev_history_files():
   history_files = os.listdir('files/historique')
 
   for history_file in history_files:
-    if (history_file != '.gitignore'):
-       os.remove(os.path.join('files/historique', history_file))
+    os.remove(os.path.join('files/historique', history_file))
--- modules/ordonnance/remove_old_ord_files.py
+++ modules/ordonnance/remove_old_ord_files.py
@@ -3,5 +3,4 @@ import os
 
 def remove_old_ord_files():
   for ordo in os.listdir('files/ordonnance'):
-    if (ordo != '.gitignore'):
-      os.remove(os.path.join('files/ordonnance', ordo))
+    os.remove(os.path.join('files/ordonnance', ordo))
