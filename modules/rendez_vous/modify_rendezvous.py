00c4df8323a99e7cbe11e1538478a6cd0e115245
yossri mhamdi <bavary1515@gmail.com>
2020-06-02 14:18:12 +0100

    [fix] udpate rdv bug.

--- modules/rendez_vous/modify_rendezvous.py
+++ modules/rendez_vous/modify_rendezvous.py
@@ -1,20 +1,14 @@
-def getIndex(rdvs, patient_id):
+def get_index(rdvs, patient_id):
   for (index, rdv) in enumerate(rdvs):
     if (patient_id == rdv.split(';')[0]):
       return index
 
-  return False
+  return -1
 
 
-def replace_rdv(rdvs, patient_id, timing):
-  index = getIndex(rdvs, patient_id)
+def replace_rdv(rdvs, patient_id, index, timing):
+  rdvs[index] = '{};{};{};\n'.format(patient_id, timing['date'], timing['time'])
 
-  if (index >= 0 and index != False) :
-    rdvs[index] = '{};{};{};\n'.format(patient_id, timing['date'], timing['time'])
-    return True
-  else:
-    print('desired rendezvous to update not found.')
-    return False
 
 
 
@@ -22,13 +16,16 @@ def modify_rendezvous(patient_id, timing):
   rdv_file = open('files/rendezvous.txt', 'r+')
   rdvs = rdv_file.readlines()
 
-  found = replace_rdv(rdvs, patient_id, timing)
+  index_to_replace = get_index(rdvs, patient_id)
 
-  if (found):
+  if (index_to_replace >= 0):
+    replace_rdv(rdvs, patient_id, index_to_replace, timing)
     rdv_file.truncate(0)
     rdv_file.seek(0)
 
     rdv_file.writelines(rdvs)
     print('done.')
+  else:
+    print('desired rendezvous to update not found.')
 
   rdv_file.close()
