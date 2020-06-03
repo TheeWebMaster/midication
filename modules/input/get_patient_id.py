75c387aa69327519f1f22c0626a0fdcc6d1848f3
yossri mhamdi <bavary1515@gmail.com>
2020-06-03 18:46:06 +0100

    added id checkout while entering and id.

--- modules/input/get_patient_id.py
+++ modules/input/get_patient_id.py
@@ -1,14 +1,18 @@
+from modules.helper.is_patient import is_patient
+
 def is_valid_id(patient_id):
   return patient_id.isdigit() and len(patient_id) == 8
 
 def get_patient_id():
   patient_id = ''
 
-  while not is_valid_id(patient_id):
+  while True:
     print('CIN doit être exactement 8 chiffre. (donner 0 pour quitter)')
     patient_id = input('CIN: ')
 
-    if patient_id == '0':
+    if is_patient(patient_id):
+      print(f'le CIN {patient_id} déjà existe')
+    elif is_valid_id(patient_id) or patient_id == '0':
       break
   
   return patient_id
